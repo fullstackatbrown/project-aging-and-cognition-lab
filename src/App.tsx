57519e78c14f389b91fa348f3ffc0299a9397708
Charlene Chen <charlenechen@Charlenes-MacBook-Air-4.local>
2024-09-28 23:34:06 -0400

    feat: created empty react app for website front end

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>This is our super cool website lol</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
