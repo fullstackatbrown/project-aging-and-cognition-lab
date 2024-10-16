fe0c5dcad2e1fb111efd4505a660583102826800
Khoi Le <leckhoi07@gmail.com>
2024-10-16 15:31:39 -0400

    in progress with mockdata and low-fi

--- src/pages/join/JoinPage.tsx
+++ src/pages/join/JoinPage.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { mockJoinData } from './join_mock_data'
 
 export default function JoinResearcherPage() {
-  return (
-    <div>JoinResearcherPage</div>
-  )
+  <div>
+    <header>Join Us Today!</header>
+    <div>
+      {/* Researcher Column */}
+      <div>
+        <h2>Join us as a researcher</h2>
+        <img>
+          src=mockJoinData.object.metadata.images.researcher_url
+          alt="Join us as a Researcher!"
+        </img>
+        <p>mockJoinData.object.metadata.text.researcher_text</p>
+        <button>As a Researcher!</button>
+      </div>
+
+      {/* Participant Column */}
+      <div>
+        <h2>Join us as a participant</h2>
+        <img>
+          src=mockJoinData.object.metadata.images.participant_url
+          alt="Join us as a Participant!"
+        </img>
+        <p>mockJoinData.object.metadata.text.paricipant_text</p>
+        <button>As a Participant!</button>
+      </div>
+    </div>
+  </div>
 }
---
+++ src/pages/join/join_mock_data.tsx
@@ -0,0 +1,16 @@
+export const mockJoinData = {
+    "object": {
+        "slug": "joinpage",
+        "title": "JoinPage",
+        "metadata": {
+            "text": {
+                "researcher_text": "https://cdn.cosmicjs.com/b60dadc0-81dd-11ef-bf3c-e1a053e2b56b-Screenshot-2024-10-03-at-7.17.41PM.png",
+                "participant_text": "https://imgix.cosmicjs.com/b60dadc0-81dd-11ef-bf3c-e1a053e2b56b-Screenshot-2024-10-03-at-7.17.41PM.png"
+            },
+            "images": {
+                "researcher_url": "https://cdn.cosmicjs.com/b60dadc0-81dd-11ef-bf3c-e1a053e2b56b-Screenshot-2024-10-03-at-7.17.41PM.png",
+                "participant_url": "https://imgix.cosmicjs.com/b60dadc0-81dd-11ef-bf3c-e1a053e2b56b-Screenshot-2024-10-03-at-7.17.41PM.png"
+            }
+        }
+    }
+}
