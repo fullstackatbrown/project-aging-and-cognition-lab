68fff2967f6c6e6815c5257dd56df164f295df7f
Khoi Le <leckhoi07@gmail.com>
2024-10-17 12:17:15 -0400

    basic lofi + mockdata

--- src/pages/join/JoinPage.tsx
+++ src/pages/join/JoinPage.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { mockJoinData } from './join_mock_data'
 
 export default function JoinResearcherPage() {
+
+  return(
   <div>
     <header>Join Us Today!</header>
     <div>
@@ -9,8 +11,8 @@ export default function JoinResearcherPage() {
       <div>
         <h2>Join us as a researcher</h2>
         <img>
-          src=mockJoinData.object.metadata.images.researcher_url
-          alt="Join us as a Researcher!"
+          {/* src=mockJoinData.object.metadata.images.researcher_url
+          alt="Join us as a Researcher!" */}
         </img>
         <p>mockJoinData.object.metadata.text.researcher_text</p>
         <button>As a Researcher!</button>
@@ -19,13 +21,14 @@ export default function JoinResearcherPage() {
       {/* Participant Column */}
       <div>
         <h2>Join us as a participant</h2>
-        <img>
-          src=mockJoinData.object.metadata.images.participant_url
-          alt="Join us as a Participant!"
+        <img src={mockJoinData.object.metadata.images.participant_url}
+        alt="Join us as a Participant!">
         </img>
         <p>mockJoinData.object.metadata.text.paricipant_text</p>
         <button>As a Participant!</button>
       </div>
     </div>
   </div>
+  );
+  
 }
