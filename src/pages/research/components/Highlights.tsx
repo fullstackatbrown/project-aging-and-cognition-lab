61a45e97f7efc4a46ea91ff20a6b89a1b9b2271d
Eric Zheng <112358392+ezheng34@users.noreply.github.com>
2024-10-19 17:52:51 -0400

    feat: general layout + placeholder images

--- src/pages/research/components/Highlights.tsx
+++ src/pages/research/components/Highlights.tsx
@@ -12,18 +12,18 @@ export default function Highlights({
   return (
     <div className="bg-gray-100 rounded-lg p-8">
       <h3 className="text-2xl font-semibold mb-6">Highlights</h3>
-      <div className="flex gap-6 bg-gray-200 rounded-lg p-6">
-        {/* Placeholder for the image */}
-        <div className="w-32 h-32 flex-shrink-0 border-2 border-gray-300 flex items-center justify-center">
+      <div className="flex gap-8 bg-gray-200 rounded-lg p-8">
+        <div className="w-48 h-48 flex-shrink-0">
           <img
             className="w-full h-full object-cover rounded-lg"
-            src="src/pages/research/images/placeholder.jpeg"
+            src="images/placeholder.jpeg"
             alt="hello"
           ></img>
         </div>
-        <div className="flex-1">
-          <h4 className="text-lg font-semibold mb-2">{highlightTitle}</h4>
-          <p className="text-gray-600">{highlightInfo}</p>
+        <div className="flex-1 py-2">
+          {" "}
+          <h4 className="text-xl font-semibold mb-3">{highlightTitle}</h4>{" "}
+          <p className="text-gray-600 text-lg">{highlightInfo}</p>{" "}
         </div>
       </div>
     </div>
