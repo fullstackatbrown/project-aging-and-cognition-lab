de8266b80624003f7b24d230895a5b483f82e30c
unknown <isaac_calderon@brown.edu>
2024-11-20 18:09:37 -0500

    feat: Header updated to match hifi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import headerImage from '../assets/image.png'
+import headerImage from '../assets/hifi.png'
+import logo from '../assets/brown_log.png'
 
 import { useNavigate } from "react-router-dom";
 
@@ -29,41 +30,59 @@ export default function Navbar() {
 
   return (
     <>
+    <div className="relative mx-auto mt-[50px] mb-[30px]">
+
+    <img 
+        src={logo} 
+        alt="Brown Logo" 
+        className="absolute top-[-31px] left-[1150px] h-[38px] w-[219px]"
+      />
+
+      
+
       <header
-        className="h-[100px] bg-contain bg-center bg-no-repeat flex flex-col items-center justify-center text-[#170303] mx-auto my-5 p-5"
+        className="h-[230px] w-[995px] bg-contain bg-left bg-no-repeat flex flex-col items-center justify-center text-[#170303] mx-auto mt-[50px] p-5 ml-[54px] mr-[231px] mb-[50px]"
         style={{ backgroundImage: `url(${headerImage})` }}
       >
+    
+         
+        
       </header>
 
-      <nav className="flex justify-center bg-gray-200 py-3">
+      
+      </div>
+
+      
+
+      <nav className="h-[61px] flex justify-center bg-[#F1DDCE] py-3">
   <button 
     onClick={() => handleNavigation("/")} 
-    className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors mr-4"
+    className=" h-[46px] w-[79px] px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors mr-4 ml-[520px]"
   >
     Home
   </button>
   <div className="flex space-x-6">
     <button 
       onClick={() => handleNavigation("/research")} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
+      className="h-[46px] w-[244px] px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
     >
       Research and Publications
     </button>
     <button 
       onClick={() => handleNavigation("/news")} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
+      className="h -[49px] w-[72px] px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
     >
       News
     </button>
     <button 
       onClick={() => handleNavigation("/members")} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
+      className="h -[49px] w-[150px] px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
     >
       Lab Members
     </button>
     <button 
       onClick={handleScrollToContacts} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
+      className=" h -[49px] w-[113px] px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors ml-[1099px]"
     >
       Contacts
     </button>
