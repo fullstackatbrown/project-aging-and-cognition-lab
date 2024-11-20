ae8b354bf118d141a93ea08e54955ade010e499c
ashleywoertz <ashley.woertz@gmail.com>
2024-11-20 17:43:15 -0500

    added in sample news data

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -16,14 +16,14 @@ export default function Article({ headline, description, date, picture, link }: 
   return (
     <div className="article flex flex-col md:flex-row md:items-center md:items-start sm:items-start sm:mt-6 sm:mb-6 md:mt-12 md:mb-12 md:space-x-8 space-y-5 md:space-y-0">
       {/* date */}
-      <div className="flex sm:flex-row md:flex-col space-x-1.5 md:space-x-0 md:space-y-1">
-        <p className="text-2xl md:text-5xl sm:text-2xl lg:text-5xl font-normal md:font-bold" style={{ color: "#D55E36" }}>
+      <div className="flex sm:flex-row md:flex-col space-x-1.5 md:space-x-0 md:space-y-1" style={{ width: "120px", flexShrink: 0 }}>
+        <p className="text-2xl md:text-5xl sm:text-2xl lg:text-5xl font-normal md:font-bold" style={{ color: "#327575" }}>
           {date.day}
         </p>
-        <p className="text-2xl leading-1 font-normal" style={{ color: "#D55E36" }}>
+        <p className="text-2xl leading-1 font-normal" style={{ color: "#327575" }}>
           {date.month}
         </p>
-        <p className="text-2xl leading-1 font-normal" style={{ color: "#D55E36" }}>
+        <p className="text-2xl leading-1 font-normal" style={{ color: "#327575" }}>
           {date.year}
         </p>
       </div>
@@ -49,7 +49,7 @@ export default function Article({ headline, description, date, picture, link }: 
       <div className="text-container align-middle">
         <div className="flex flex-col space-x-0">
           <div className="flex flex-col space-y-3 md:space-y-6">
-            <h2 className="text-xl md:text-2xl" style={{ color: "#D55E36" }}>
+            <h2 className="text-xl md:text-2xl" style={{ color: "#51AAAB" }}>
               {headline}
             </h2>
             <p className="text-base md:text-lg leading-5">{description}</p>
