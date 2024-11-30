b3f6bb88149c5418ab74880eb2eba9f696f80aa1
Kei Watanabe <kei_watanabe@brown.edu>
2024-11-30 15:02:31 -0500

    mock data replaced

--- src/components/HomeResearchPrev.tsx
+++ src/components/HomeResearchPrev.tsx
@@ -5,8 +5,8 @@ export default function HomeResearchPrev(props: any) {
 
     <div className="shadow-md bg-gray-100 w-full h-full p-4 rounded-3xl space-y-2 group-hover: bg-gray-200
     transition ease-in-out delay-50 hover:bg-gray-300 flex flex-col justify-center items-center">
-        <h4 className='text-center text-lg'>{props.title}</h4>
-        <h5 className='text-center text-md'>{props.blurb}</h5>
+        <h4 className='text-center text-lg font-bold'>{props.title}</h4>
+        <h5 className='text-center text-sm'>{props.blurb}</h5>
         <div className='flex justify-center items-center w-1/3'>
           <img className='rounded-lg shadow-md'
           src={props.imageURL} alt=""/>
