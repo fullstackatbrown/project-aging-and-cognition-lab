5dc331600f213f2e0f8532b3faed4bc8ad6f4db7
Charlene Chen <charlenechen@Charlenes-MacBook-Air-7.local>
2024-12-03 09:22:00 -0500

    home, research, news page completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ export default function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const navigate = useNavigate(); // Use useNavigate for programmatic navigation
   const logo = require('../assets/banner.png');
+  const brownLogo = require('../assets/brown_logo.png');
 
   // Toggle dropdown
   const toggleDropdown = () => {
@@ -28,46 +29,51 @@ export default function Navbar() {
 
   return (
     <>
-      <header
-        className="h-[175px] bg-contain bg-center bg-no-repeat flex flex-col items-left justify-center text-[#170303] mx-auto my-5 p-10"
-      >
+      <header className="h-[175px] bg-contain bg-center bg-no-repeat flex flex-col items-left justify-center text-[#170303] mx-auto my-5 p-10">
         <img src={logo} className="object-contain" alt="Logo"/>
+        <img 
+          src={brownLogo} 
+          alt="Brown Logo" 
+          className="absolute top-0 right-0 w-[20rem]" 
+        />
       </header>
 
-      <nav className="flex justify-center bg-gray-200 py-3">
-  <button 
-    onClick={() => handleNavigation("/")} 
-    className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors mr-4"
-  >
-    Home
-  </button>
-  <div className="flex space-x-6">
-    <button 
-      onClick={() => handleNavigation("/research")} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
-    >
-      Research and Publications
-    </button>
-    <button 
-      onClick={() => handleNavigation("/news")} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
-    >
-      News
-    </button>
-    <button 
-      onClick={() => handleNavigation("/members")} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
-    >
-      Lab Members
-    </button>
-    <button 
-      onClick={handleScrollToContacts} 
-      className="px-4 py-2 text-black rounded-md hover:bg-blue-700 transition-colors"
-    >
-      Contacts
-    </button>
-  </div>
-</nav>
+      <nav className="flex flex-row justify-end align-center bg-[#F1DDCE] my-5 py-4 px-5">
+        <button 
+          onClick={() => handleNavigation("/")} 
+          className="flex px-4 py-2 text-black rounded-md hover:bg-[#d6beab] transition-colors mr-4"
+        >
+          Home
+        </button>
+
+        <button 
+          onClick={() => handleNavigation("/research")} 
+          className="flex px-4 py-2 text-black rounded-md hover:bg-[#d6beab] transition-colors mr-4"
+        >
+          Research & Publications
+        </button>
+
+        <button 
+          onClick={() => handleNavigation("/news")} 
+          className="flex px-4 py-2 text-black rounded-md hover:bg-[#d6beab] transition-colors mr-4"
+        >
+          News
+        </button>
+
+        <button 
+          onClick={() => handleNavigation("/members")} 
+          className="flex px-4 py-2 text-black rounded-md hover:bg-[#d6beab] transition-colors mr-4"
+        >
+          Lab Members
+        </button>
+
+        <button 
+          onClick={handleScrollToContacts} 
+          className="flex px-4 py-2 text-black rounded-md hover:bg-[#d6beab] transition-colors"
+        >
+          Contacts
+        </button>
+      </nav>
     </>
   );
 }
