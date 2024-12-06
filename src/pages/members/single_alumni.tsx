81cb2e8cb1e4de72470b54c4d9a59d88d2dcdb7a
jli02 <113394184+jli02@users.noreply.github.com>
2024-12-06 11:52:07 -0500

    fixed alumni photos

--- src/pages/members/single_alumni.tsx
+++ src/pages/members/single_alumni.tsx
@@ -9,20 +9,29 @@ export interface AlumniProps {
 }
 
 export function Alumni(props: AlumniProps) {
-  const alumni1 = require("./photos/Alicia_gradstudent_alum.png");
+  //const alumni1 = require("./photos/Alicia_gradstudent_alum.png");
 
-  // function makePhotoURL(email: string): any {
-  //   const pngURL = require(props.photo);
-  //   return pngURL;
-  // }
+  const photos: { [key: string]: string } = {
+    alum1: require("./photos/Alicia_gradstudent_alum.png"),
+    alum2: require("./photos/BethKellerman_gradstudent_alum.png"),
+    alum3: require("./photos/Diah_ugrad_alum.png"),
+  };
+
+  function makePhotoURL(photoKey: string): string {
+    return photos[photoKey] || "";
+  }
 
   return (
     <div className="single-alumni">
-      <img src={alumni1} alt="alumni picture" className="photo" />
+      <img
+        src={makePhotoURL(props.photo)}
+        alt="alumni picture"
+        className="photo"
+      />
 
-      <div>
+      <div className="alumni-description">
         <h2 aria-label="alumni name">{props.name}</h2>
-        <h3 aria-label="role">{props.role}</h3>
+        {/* <h3 aria-label="role">{props.role}</h3> */}
         <p aria-label="alumni description">{props.description}</p>
       </div>
     </div>
