07e28dbe90a48d763bdacd980be4d63bb116e881
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2025-01-18 21:57:23 -0500

    make button more extensible

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,7 @@
 export default function Button({ text, className }: any) {
-    return (
-        <button className="transition ease-in-out delay-50 hover:bg-gray-300 bg-white border border-cyan-600 border-2 py-3 px-10 rounded-full text-xl">{text}</button>
-    )
+  return (
+    <button className={`transition ease-in-out delay-50 text-xl ${className}`}>
+      {text}
+    </button>
+  );
 }
