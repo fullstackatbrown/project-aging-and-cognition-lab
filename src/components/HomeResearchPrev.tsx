329730272ffac8840b65ebdab7f1898591634ef7
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2025-01-18 21:58:09 -0500

    revise research previews style

--- src/components/HomeResearchPrev.tsx
+++ src/components/HomeResearchPrev.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default function HomeResearchPrev(props: any) {
   return (
-
-    <div className="shadow-md bg-gray-100 w-full h-full p-4 rounded-3xl space-y-2 group-hover: bg-gray-200
-    transition ease-in-out delay-50 hover:bg-gray-300 flex flex-col justify-center items-center">
-        <h4 className='text-center text-lg font-bold'>{props.title}</h4>
-        <h5 className='text-center text-sm'>{props.blurb}</h5>
-        <div className='flex justify-center items-center w-1/3'>
-          <img className='rounded-lg shadow-md'
-          src={props.imageURL} alt=""/>
-        </div>
-       
+    <div
+      className="bg-gray-100 w-full h-full p-4 rounded-3xl space-y-2 group-hover: bg-gray-200
+    transition ease-in-out delay-50 hover:bg-gray-300 flex flex-col justify-center items-center"
+    >
+      <h4 className="leading-6 text-center text-lg font-bold mb-2">
+        {props.title}
+      </h4>
+      <h5 className="text-center text-sm">{props.blurb}</h5>
+      <div className="flex justify-center items-center w-1/2">
+        <img className="rounded-lg mt-2" src={props.imageURL} alt="" />
+      </div>
     </div>
-  )
+  );
 }
